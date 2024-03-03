6616f24a7d46ccb51c61780131a2a525c9de1164
Sirisiva1998 <Sirisiva1998@gmail.com>
2024-03-03 23:04:54 +0530

    Server setup

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const express=require("express");
 const cors=require("cors");
 const dotenv=require('dotenv').config({path:"./config.env"});
+
+//Setup app
+const app=express();
+
+app.use(cors());
+
+app.listen(process.env.PORT || 2600,process.env.host,()=>{console.log(`Server running on port ${process.env.host}`)});
